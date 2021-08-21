1c0a233ec27e4046b0a6f304dc79fa3af7a5ea9d
Aleksey Sasovets <aleksey.sasovets@gmail.com>
2021-08-21 16:58:50 +0700

    HW07 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,83 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrInvalidFileNames      = errors.New("invalid file names")
 )
 
-func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+func Copy(src, dst string, offset, limit int64) error {
+	if src == "" || dst == "" {
+		return ErrInvalidFileNames
+	}
+
+	srcStat, err := os.Stat(src)
+	if err != nil {
+		return err
+	}
+
+	if !srcStat.Mode().IsRegular() {
+		return ErrUnsupportedFile
+	}
+
+	if offset > srcStat.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	iLimit := int(limit)
+	bufSize := int(srcStat.Size())
+
+	if limit+offset > srcStat.Size() {
+		iLimit = int(srcStat.Size() - offset)
+	}
+
+	if iLimit > 0 && iLimit < bufSize {
+		bufSize = iLimit
+	}
+
+	buf := make([]byte, bufSize)
+
+	reader, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer reader.Close()
+
+	writer, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer writer.Close()
+
+	n, err := reader.ReadAt(buf, offset)
+	if err != nil {
+		if !errors.Is(err, io.EOF) {
+			return err
+		}
+	}
+
+	if offset > 0 {
+		_, err = reader.Seek(offset, 0)
+		if err != nil {
+			return err
+		}
+	}
+
+	bar := pb.Full.Start(bufSize)
+	barReader := bar.NewProxyReader(reader)
+
+	if _, err := io.CopyN(writer, barReader, int64(n)); err != nil {
+		return err
+	}
+
+	bar.Finish()
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,43 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const OutFile = "out.txt"
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("blank file names", func(t *testing.T) {
+		err := Copy("", "", 0, 0)
+		assert.Equal(t, ErrInvalidFileNames, err)
+
+		t.Cleanup(func() { removeOutFile() })
+	})
+
+	t.Run("random dev", func(t *testing.T) {
+		err := Copy("/dev/urandom", OutFile, 0, 0)
+		assert.Equal(t, ErrUnsupportedFile, err)
+
+		t.Cleanup(func() { removeOutFile() })
+	})
+
+	t.Run("limit is bigger than file size", func(t *testing.T) {
+		srcFile := "testdata/input.txt"
+		srcStat, err := os.Stat(srcFile)
+		if err != nil {
+			t.Fatal("getting file stat failed")
+		}
+
+		err = Copy(srcFile, OutFile, srcStat.Size()+1, 0)
+		assert.Equal(t, ErrOffsetExceedsFileSize, err)
+
+		t.Cleanup(func() { removeOutFile() })
+	})
+}
+
+func removeOutFile() {
+	_ = os.Remove(OutFile)
 }
