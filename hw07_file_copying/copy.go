f521c7f2d869db084574c01806a298183aafda85
Aleksey Sasovets <aleksey.sasovets@gmail.com>
2021-09-11 10:50:00 +0700

    HW07 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"io"
 	"os"
 
@@ -21,7 +22,7 @@ func Copy(src, dst string, offset, limit int64) error {
 
 	srcStat, err := os.Stat(src)
 	if err != nil {
-		return err
+		return fmt.Errorf("os.Stat: %w", err)
 	}
 
 	if !srcStat.Mode().IsRegular() {
@@ -32,50 +33,49 @@ func Copy(src, dst string, offset, limit int64) error {
 		return ErrOffsetExceedsFileSize
 	}
 
-	iLimit := int(limit)
-	bufSize := int(srcStat.Size())
+	bufSize := srcStat.Size()
 
-	if limit+offset > srcStat.Size() {
-		iLimit = int(srcStat.Size() - offset)
+	if limit > srcStat.Size()-offset {
+		limit = srcStat.Size() - offset
 	}
 
-	if iLimit > 0 && iLimit < bufSize {
-		bufSize = iLimit
+	if limit > 0 && limit < bufSize {
+		bufSize = limit
 	}
 
 	buf := make([]byte, bufSize)
 
 	reader, err := os.Open(src)
 	if err != nil {
-		return err
+		return fmt.Errorf("os.Open: %w", err)
 	}
 	defer reader.Close()
 
 	writer, err := os.Create(dst)
 	if err != nil {
-		return err
+		return fmt.Errorf("os.Create: %w", err)
 	}
 	defer writer.Close()
 
 	n, err := reader.ReadAt(buf, offset)
 	if err != nil {
 		if !errors.Is(err, io.EOF) {
-			return err
+			return fmt.Errorf("reader.ReadAt: %w", err)
 		}
 	}
 
 	if offset > 0 {
 		_, err = reader.Seek(offset, 0)
 		if err != nil {
-			return err
+			return fmt.Errorf("reader.Seek: %w", err)
 		}
 	}
 
-	bar := pb.Full.Start(bufSize)
+	bar := pb.Full.Start64(bufSize)
 	barReader := bar.NewProxyReader(reader)
 
 	if _, err := io.CopyN(writer, barReader, int64(n)); err != nil {
-		return err
+		return fmt.Errorf("io.CopyN: %w", err)
 	}
 
 	bar.Finish()
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,43 +1,51 @@
 package main
 
 import (
+	"log"
 	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-const OutFile = "out.txt"
+const outFile = "copy_test_out.txt"
 
 func TestCopy(t *testing.T) {
 	t.Run("blank file names", func(t *testing.T) {
 		err := Copy("", "", 0, 0)
 		assert.Equal(t, ErrInvalidFileNames, err)
-
-		t.Cleanup(func() { removeOutFile() })
 	})
 
 	t.Run("random dev", func(t *testing.T) {
-		err := Copy("/dev/urandom", OutFile, 0, 0)
-		assert.Equal(t, ErrUnsupportedFile, err)
+		f := createOutFile()
+		t.Cleanup(func() { removeOutFile(f) })
 
-		t.Cleanup(func() { removeOutFile() })
+		err := Copy("/dev/urandom", f.Name(), 0, 0)
+		assert.Equal(t, ErrUnsupportedFile, err)
 	})
 
 	t.Run("limit is bigger than file size", func(t *testing.T) {
+		f := createOutFile()
+		t.Cleanup(func() { removeOutFile(f) })
+
 		srcFile := "testdata/input.txt"
 		srcStat, err := os.Stat(srcFile)
-		if err != nil {
-			t.Fatal("getting file stat failed")
-		}
+		require.NoError(t, err)
 
-		err = Copy(srcFile, OutFile, srcStat.Size()+1, 0)
+		err = Copy(srcFile, f.Name(), srcStat.Size()+1, 0)
 		assert.Equal(t, ErrOffsetExceedsFileSize, err)
-
-		t.Cleanup(func() { removeOutFile() })
 	})
 }
 
-func removeOutFile() {
-	_ = os.Remove(OutFile)
+func removeOutFile(file *os.File) {
+	_ = os.Remove(file.Name())
+}
+
+func createOutFile() *os.File {
+	f, err := os.CreateTemp("", outFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return f
 }
